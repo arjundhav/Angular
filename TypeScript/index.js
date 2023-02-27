5deebcaa7e5a5764526d8541b073c018591842c1
Arjun <arjundhav@gmail.com>
2023-02-27 10:19:46 +0530

    first commit

---
+++ TypeScript/index.js
@@ -0,0 +1,6 @@
+var a = 20;
+console.log(a);
+var b = "hello world !!";
+console.log(b);
+var c = [1, 2, 'Arjun', true];
+console.log(c);
---
+++ TypeScript/index.ts
@@ -0,0 +1,8 @@
+var a:number = 20
+console.log(a);
+
+var b:string = "hello world !!";
+console.log(b);
+
+var c:any[] = [1,2,'Arjun',true]
+console.log(c);
