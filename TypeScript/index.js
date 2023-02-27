b895aea2f66c431cfd50a071d2de0ff78189c8be
Arjun <arjundhav@gmail.com>
2023-02-27 10:52:34 +0530

    slight change

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -2,5 +2,14 @@ var a = 20;
 console.log(a);
 var b = "hello world !!";
 console.log(b);
-var c = [1, 2, 'Arjun', true];
+var c = [1, 2, "Arjun", true];
 console.log(c);
+function abc() {
+  console.log("ABC");
+}
+abc();
+function xyz(a, b) {
+  return a * b;
+}
+var v = xyz(2, 3);
+console.log(v);
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,8 +1,20 @@
-var a:number = 20
+var a: number = 20
 console.log(a);
 
-var b:string = "hello world !!";
+var b: string = "hello world !!";
 console.log(b);
 
-var c:any[] = [1,2,'Arjun',true]
+var c: any[] = [1, 2, 'Arjun', true]
 console.log(c);
+
+function abc(): void {
+    console.log('ABC')
+}
+abc()
+
+function xyz(a: number, b: number) {
+    return a * b
+}
+var v = xyz(2, 3)
+console.log(v)
+
